4451f781eaf3cf423a649cbfb80faf57c9494e3b
Dina Getta <enularu@gmail.com>
2021-11-30 16:20:29 +0300

    file change

--- Program.cs
+++ Program.cs
@@ -10,18 +10,7 @@ namespace VehicleFleet
     class Program
     {
         static void Main(string[] args)
-        {
-            ElectricalEngine a = new ElectricalEngine(12,53,"type1");
-           
-            FourWheeleChassi b = new FourWheeleChassi("type2", 12);
-            
-            MechanicalTransmission c = new MechanicalTransmission(35, "type3");
-            Car d = new Car(b, a, c, 234.2, true, 3);
-            Console.WriteLine(d.ToString());
-
-
-
-
+        {            
         }
     }
 }
